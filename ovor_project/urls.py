8c35eec225491a7bf4f244110e90385438bc8fc4
Sanjay Jadhav <132054835+jadhavS123@users.noreply.github.com>
2025-11-01 17:48:13 +0530

    Update urls.py

--- ovor_project/urls.py
+++ ovor_project/urls.py
@@ -1,6 +1,29 @@
+# """
+# URL configuration for ovor_project project.
+
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/5.2/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('mgnrega.urls')),
+# ]
+
 """
 URL configuration for ovor_project project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
@@ -16,8 +39,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        'message': 'Welcome to Our Voice, Our Rights API',
+        'status': 'API is running successfully',
+        'endpoints': {
+            'districts': '/api/districts/',
+            'performance': '/api/performance/<district_name>/',
+            'admin_panel': '/admin/'
+        },
+        'documentation': 'Access the endpoints above to retrieve MGNREGA data'
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('api/', include('mgnrega.urls')),
 ]
